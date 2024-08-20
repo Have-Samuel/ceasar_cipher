f0201674138bec8a6c25e53e450de9475cc01571
samuel <samiehave9@gmail.com>
2024-08-20 12:25:43 +0300

    Commenting

--- ceasar_C.rb
+++ ceasar_C.rb
@@ -1,31 +1,29 @@
+# # str = "What a string!"
+# # str.to_i
 
+# # (position_index + shift) % 26, will be formula we will use to shift the letters
 
-# str = "What a string!"
-# str.to_i
-
-# (position_index + shift) % 26, will be formula we will use to shift the letters
-
-def ceasar_cipher(str, shift)
-  # empty array to store the shifted letters
-  shifted_letters = []
-  # split the string into an array of characters
-  str.split("").each do |letter|
-    # we will use ASCII values to shift the letters
-    # check if the letter is a capital letter
-    # What does the .ord method do? It returns the ASCII value of the character it is called on.
-    if letter.ord >= 65 && letter.ord <= 90
-      shifted_letter = (letter.ord + shift - 65) % 26 + 65
-      shifted_letters.push(shifted_letter.chr)
-    # check if the letter is a lower case letter
-    elsif letter.ord >= 97 && letter.ord <= 122
-      shifted_letter = (letter.ord + shift - 97) % 26 + 97
-      shifted_letters.push(shifted_letter.chr)
-    # if the letter is not a letter, push it to the array
-    else
-      shifted_letters.push(letter)
-    end
-  end
-end
+# def ceasar_cipher(str, shift)
+#   # empty array to store the shifted letters
+#   shifted_letters = []
+#   # split the string into an array of characters
+#   str.split("").each do |letter|
+#     # we will use ASCII values to shift the letters
+#     # check if the letter is a capital letter
+#     # What does the .ord method do? It returns the ASCII value of the character it is called on.
+#     if letter.ord >= 65 && letter.ord <= 90
+#       shifted_letter = (letter.ord + shift - 65) % 26 + 65
+#       shifted_letters.push(shifted_letter.chr)
+#     # check if the letter is a lower case letter
+#     elsif letter.ord >= 97 && letter.ord <= 122
+#       shifted_letter = (letter.ord + shift - 97) % 26 + 97
+#       shifted_letters.push(shifted_letter.chr)
+#     # if the letter is not a letter, push it to the array
+#     else
+#       shifted_letters.push(letter)
+#     end
+#   end
+# end
 
 def cipher(str, shift)
   # Looping arrays
@@ -33,13 +31,18 @@ def cipher(str, shift)
   ciphier_ord_array = []
   ciphier_array = []
 
-  # Convert string to downcase and array to strings
+  # Convert string to downcase and then, string to array of characters.
+  # And include spaces and punctuationi using chars method
   str_array = str.downcase.chars
+  # ["w", "h", "a", "t", " ", "a", " ", "s", "t", "r", "i", "n", "g", "!"]
 
   # Create array of srting ordinal values
   for i in str_array
     ord_array.push(i.ord)
   end
+  # The ord method gives each character in the string a number value and stores it in an array
+  # [119, 104, 97, 116, 32, 97, 32, 115, 116, 114, 105, 110, 103, 33]
+  # 119 = w, 104 = h, 97 = a, 116 = t, 32 = space, 97 = a, 32 = space, 115 = s, 116 = t, 114 = r, 105 = i, 110 = n, 103 = g, 33 = !
 
   # first push i not equal to a-z to final product, no mutation
   # second Wrap from a to z in case i - shift is normal below 'a'
