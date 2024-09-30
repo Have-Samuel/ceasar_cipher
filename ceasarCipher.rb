88d2142e2dd6975e7145235f02229ea368c214da
samuel <samiehave9@gmail.com>
2024-09-30 13:23:06 +0300

    Create a file and a new function

---
+++ ceasarCipher.rb
@@ -0,0 +1,6 @@
+function rot13(encodedString) {
+  let codeArr = encodedString.split(''); # split the string into an array
+  let decodedArr = []; # create an empty array to store the decoded values
+  # Create an aphalbet array that goes past Z by 13 characters starting from A
+  let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+}
